bb84b837332e7915f6a39e7e81f56ef0bb6f24e0
jzuo <maxime.zuo@factset.com>
2020-01-15 17:02:41 +0100

    upgrade libs

--- src/Repository/DepenseRepository.php
+++ src/Repository/DepenseRepository.php
@@ -3,18 +3,18 @@ namespace App\Repository;
 
 use App\Entity\Depense;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 class DepenseRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Depense::class);
     }
 
 
     /**
-     * 
+     *
      * @return Depense[]
      */
     public function findAllOrderByDateBuy(): array
--- src/Repository/ShopRepository.php
+++ src/Repository/ShopRepository.php
@@ -3,18 +3,18 @@ namespace App\Repository;
 
 use App\Entity\Shop;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ShopRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Shop::class);
     }
 
 
     /**
-     * 
+     *
      * @return Shop[]
      */
     public function findAllOrderByName(): array
